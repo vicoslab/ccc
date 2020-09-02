7c2501f43a57bbd23211239c0e43b60e8b49a47b
Domen Tabernik <domen.tabernik@fri.uni-lj.si>
2020-09-02 18:49:14 +0200

    Added option to specificy name of APPS in X11 images

--- x11/etc/runit_init.d/08_register_apps.sh
+++ x11/etc/runit_init.d/08_register_apps.sh
@@ -11,12 +11,20 @@ IFS=':' read -ra APPS_ARRAY <<< "$APPS"
 for app_path in "${APPS_ARRAY[@]}"; do
     # remove any trailing spaces
     app_path=`echo $app_path | xargs`
+
+    # get provided filename if comma is found
+    IFS=',' read -ra app_name <<< "$app_path"
     
-    # get filename only
-    filename=$(basename -- "$app_path") 
+    app_path=${app_name[0]}
+    filename=${app_name[1]}
     
-    # remove extension 
-    filename="${filename%.*}"
+    # get filename from path if not provided directly
+    if [ -z "$filename" ]; then
+        filename=$(basename -- "$app_path") 
+    
+        # remove extension 
+        filename="${filename%.*}"
+    fi
     
     # register symlink to /usr/local/bin
     ln -s $app_path "/usr/local/bin/$filename"
