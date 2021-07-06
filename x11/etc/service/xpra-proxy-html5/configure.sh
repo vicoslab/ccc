c8771ca033f93d3271f2787b76fa445807c9df1f
Domen Tabernik <domen.tabernik@fri.uni-lj.si>
2021-07-06 02:18:30 +0200

    Added HTML5 version of xpra proxy to X11 image

---
+++ x11/etc/service/xpra-proxy-html5/configure.sh
@@ -0,0 +1,18 @@
+#!/bin/sh -e
+
+USER_NAME=${USER_NAME:-user}
+USER_HOME=${BASE%"/"}/$USER_NAME
+
+# need to set HOME to user 
+export HOME=$USER_HOME
+
+LOG_DIR=$USER_HOME/.xpra
+
+if [ -z "${XPRA_PROXY_HTML5_PORT}" ]; then
+	exit 1
+fi
+
+mkdir -p $LOG_DIR
+
+# wait a few sec to ensure dbus is started
+while [ -z "$(sv status dbus | grep pid)" ] ; do sleep 3; done
