f1cecdd8af067e3bf9e3477cd4fef094a04943db
Domen Tabernik <domen.tabernik@fri.uni-lj.si>
2021-07-06 02:20:35 +0200

    Added new image that provides JetBrains projector as a main server

---
+++ jetbrains-projector/etc/runit_init.d/08_jetbrains_projector.sh
@@ -0,0 +1,82 @@
+#!/bin/bash
+
+USER_NAME=${USER_NAME:-user}
+USER_HOME=${BASE%"/"}/$USER_NAME
+USER_ID=`id -u $USER_NAME`
+
+export HOME=$USER_HOME
+
+CONDA_BIN=$USER_HOME/conda/bin/conda
+CONDA_PROJECTOR_ENV=jetbrains-projector
+
+PROJECTOR_BIN="${CONDA_BIN} run -n ${CONDA_PROJECTOR_ENV} projector"
+
+export PROJECTOR_IDE=${PROJECTOR_IDE:-"PyCharm Professional Edition 2021.1.3"}
+export PROJECTOR_PORT=${PROJECTOR_PORT:-9999}
+export PROJECTOR_CONFIG=${PROJECTOR_CONFIG:-default}
+PROJECTOR_CONFIG_FILE=$USER_HOME/.projector/configs/$PROJECTOR_CONFIG/config.ini
+
+ensure_secure_config() {
+
+	DO_UPDATE=""
+	
+	AUTH_TOKEN=$(grep -o "^password = .*" $PROJECTOR_CONFIG_FILE | cut -f2- -d=)
+	
+	if [ -z "$AUTH_TOKEN" ]; then
+		# auth token not configure: create new one
+		AUTH_TOKEN=`date +%s | sha256sum | base64 | head -c 64`
+		
+		cat >> $PROJECTOR_CONFIG_FILE <<- EOF
+		[PASSWORDS]
+		password = $AUTH_TOKEN
+		ro_password = $AUTH_TOKEN
+		EOF
+		
+		DO_UPDATE=1
+	fi
+	
+	SSL_TOKEN=$(grep -o "^token = .*" $PROJECTOR_CONFIG_FILE | cut -f2- -d=)
+	
+	if [ -z "$SSL_TOKEN" ]; then
+		# SSL no configure: create new token
+		SSL_TOKEN=`date +%s | sha256sum | base64 | head -c 20`
+		
+		cat >> $PROJECTOR_CONFIG_FILE <<- EOF
+		[SSL]
+		token = $SSL_TOKEN
+		EOF
+		
+		DO_UPDATE=1
+	fi
+	
+	if [ ! -z "$DO_UPDATE" ]; then
+		chpst -u $USER_NAME $PROJECTOR_BIN config rebuild
+	fi 
+}
+
+# install jetbrains-projector into conda enviroment
+if [ -z "$(${CONDA_BIN} env list | grep '^${CONDA_PROJECTOR_ENV} */')" ]; then
+	
+	echo "Installing conda env $CONDA_PROJECTOR_ENV"
+	chpst -u $USER_NAME $CONDA_BIN create --name $CONDA_PROJECTOR_ENV --file /etc/jetbrains-projector-requirements.txt -y
+	chpst -u $USER_NAME $CONDA_BIN run -n $CONDA_PROJECTOR_ENV pip install projector-installer
+	echo "  (done)"	
+fi 
+
+# put empty folder to prevent GPL question when running projector for the first time
+chpst -u $USER_NAME mkdir -p $USER_HOME/.projector/apps
+chpst -u $USER_NAME mkdir -p $USER_HOME/.projector/configs
+chpst -u $USER_NAME mkdir -p $USER_HOME/.projector/cache
+
+if [ -z "$($PROJECTOR_BIN config list | grep $PROJECTOR_CONFIG)" ]; then
+	
+	# install default app
+	echo "Installing '$PROJECTOR_IDE' as JetBrains projector app under config name '$PROJECTOR_CONFIG' (port: $PROJECTOR_PORT)"
+	chpst -u $USER_NAME $PROJECTOR_BIN autoinstall --config-name $PROJECTOR_CONFIG --ide-name "$PROJECTOR_IDE" --port $PROJECTOR_PORT --hostname 0.0.0.0
+	chpst -u $USER_NAME $PROJECTOR_BIN config update $PROJECTOR_CONFIG
+	echo "  (done)"
+	
+	# enable SSL and add tokens it not present
+	ensure_secure_config
+
+fi
---
+++ jetbrains-projector/etc/service/jetbrains-projector/configure.sh
@@ -0,0 +1,170 @@
+#!/bin/sh -e
+
+#####################################################################################
+# variable setup 
+
+USER_NAME=${USER_NAME:-user}
+USER_HOME=${BASE%"/"}/$USER_NAME
+
+export HOME=$USER_HOME
+
+CONDA_BIN=$USER_HOME/conda/bin/conda
+CONDA_PROJECTOR_ENV=jetbrains-projector
+
+PROJECTOR_BIN="${CONDA_BIN} run -n ${CONDA_PROJECTOR_ENV} projector"
+PROJECTOR_CONFIG=${PROJECTOR_CONFIG:-default}
+PROJECTOR_CONFIG_FOLDER=$USER_HOME/.projector/configs/$PROJECTOR_CONFIG/
+PROJECTOR_CONFIG_FILE=$PROJECTOR_CONFIG_FOLDER/config.ini
+
+LOG_DIR=$USER_HOME/.projector
+
+mkdir -p $LOG_DIR
+
+#####################################################################################
+# function defines
+
+ensure_secure_config() {
+
+	DO_UPDATE=""
+	
+	# find authentication token (should be in section [PASSWORDS] and starts with 'password = ')
+	AUTH_TOKEN=$(grep -o "^password = .*" $PROJECTOR_CONFIG_FILE | cut -f2- -d=)
+	
+	# if auth token not configure then create new one
+	if [ -z "$AUTH_TOKEN" ]; then
+		AUTH_TOKEN=`date +%s | sha256sum | base64 | head -c 64`
+		
+		cat >> $PROJECTOR_CONFIG_FILE <<- EOF
+		[PASSWORDS]
+		password = $AUTH_TOKEN
+		ro_password = $AUTH_TOKEN
+		EOF
+		
+		DO_UPDATE=1
+	fi
+	
+	# find SSL token (should be in section [SSL] and starts with 'token = ')
+	SSL_TOKEN=$(grep -o "^token = .*" $PROJECTOR_CONFIG_FILE | cut -f2- -d=)
+	
+	# if SSL is not configured then create new token
+	if [ -z "$SSL_TOKEN" ]; then
+		SSL_TOKEN=`date +%s | sha256sum | base64 | head -c 20`
+		
+		cat >> $PROJECTOR_CONFIG_FILE <<- EOF
+		[SSL]
+		token = $SSL_TOKEN
+		EOF
+		
+		DO_UPDATE=1
+	fi
+	
+	# rebuild config file if changes were made
+	if [ ! -z "$DO_UPDATE" ]; then
+		chpst -u $USER_NAME $PROJECTOR_BIN config rebuild
+	fi 
+}
+
+remove_auth_token_motd() {
+	# remove existing token from MOTD 
+	line=$(grep -n 'TOKEN (JetBrains Projector)' /etc/motd | cut -f1 -d:)
+	if [ ! -z "$line" ]; then
+		sed -i "$((line))d" /etc/motd
+	fi 
+}
+
+add_auth_token_motd() {
+	# first remove any existing tokens 
+	remove_auth_token_motd
+
+	# find actual token
+	AUTH_TOKEN=$(grep -o "^password = .*" $PROJECTOR_CONFIG_FILE | cut -f2- -d=)
+	
+	# save value that will be appended to MOTD into temp file
+	cat > /tmp/motd.jetbrains <<- EOF
+	TOKEN (JetBrains Projector): ${AUTH_TOKEN}
+	EOF
+	
+	# append right before CONTAINER STATUS
+	line=$(grep -n "CONTAINER STATUS:" /etc/motd | cut -f1 -d:)
+	sed -i "$((line-1))r/tmp/motd.jetbrains" /etc/motd
+	
+	# remove temp file
+	rm /tmp/motd.jetbrains
+}
+
+clenup_previous_run() {
+
+	# need to manually kill java app for projector 
+	pkill -f "${USER_HOME}/.projector/apps/.*/java.*" 2> /dev/null || echo ""
+
+	# also clear the lock flag
+	rm "$PROJECTOR_CONFIG_FOLDER/run.lock" 2> /dev/null || echo ""
+}
+
+remove_warning_motd() {
+	# if warning is in MOTD file them revert it back to original (/tmp/motd.back)
+	if [ ! -z "$(grep 'JetBrains Projector IS NOT RUNNING' /etc/motd)" ]; then 
+		cp /tmp/motd.back /etc/motd && rm /tmp/motd.back
+	fi
+}
+
+insert_warning_motd() {
+	# if any previous TOKENs were written in motd then remove them since they will not be valid any more
+	remove_auth_token_motd
+
+	# make a copy of motd to which we can restore when projector is properly configured and started
+	cp /etc/motd /tmp/motd.back
+	
+	# create temp file with message
+	cat > /tmp/motd.jetbrains <<- EOF
+	
+	#######################################################################
+	WARNING: JetBrains Projector IS NOT RUNNING due to missing config
+	
+		Please run following to fully configure: 
+			 > conda activate jetbrains-projector
+			 > projector ide install --no-auto-run
+			 > projector config add ${PROJECTOR_CONFIG}
+		
+		Use following settings:
+			- host: 0.0.0.0
+			- port: ${PROJECTOR_PORT:-9999}
+			
+	#######################################################################
+	EOF
+
+	# insert message before "CONTAINER STATUS" line
+	line=$(grep -n "CONTAINER STATUS:" /etc/motd | cut -f1 -d:)
+	sed -i "${line}r/tmp/motd.jetbrains" /etc/motd
+	
+	# remove temp file
+	rm /tmp/motd.jetbrains
+
+}
+#####################################################################################
+# main checks and configuration
+
+## clean-up any child processes that did not exit properly from previous run
+# must be done before calling projector config list
+clenup_previous_run
+
+## check if valid configuration for projector exists
+if [  -z "$($PROJECTOR_BIN config list | grep $PROJECTOR_CONFIG)" ]; then
+
+	# insert warning into MOTD if not present jet
+	if [ -z "$(grep 'JetBrains Projector IS NOT RUNNING' /etc/motd)" ]; then
+		insert_warning_motd
+	fi
+
+	# return error since we cannot continue (this will throw exception in run)
+	exit 1
+fi
+
+## remove missing config message from MOTD
+remove_warning_motd
+
+## ensure that config is secure (using secure websockets and token)
+ensure_secure_config
+
+## append authentication token to MOTD
+add_auth_token_motd	
