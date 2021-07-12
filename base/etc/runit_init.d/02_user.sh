1108fceb4cb6f911628d759517d2c66288cdacfe
Domen Tabernik <domen.tabernik@fri.uni-lj.si>
2021-07-12 15:48:14 +0200

    Added moving of existing .projector into backup folder and updated (for .cache and .projector) to add random suffix when moving folder

--- base/etc/runit_init.d/02_user.sh
+++ base/etc/runit_init.d/02_user.sh
@@ -82,12 +82,17 @@ if [ ! -z "$LOCAL_SSD_STORAGE" ]; then
     
     # check if existing folder is not symlink but actual folder -- rename it in case user has something important there
     if [ -d "$USER_HOME/.cache" ] && [ ! -h "$USER_HOME/.cache" ] ; then
-        chpst -u $USER_NAME mv "$USER_HOME/.cache" "$USER_HOME/.cache_backup"
+        RANDOM_SUFFIX=$(date +%s | sha256sum | base64 | head -c 4)
+        chpst -u $USER_NAME mv "$USER_HOME/.cache" "$USER_HOME/.cache_backup_$RANDOM_SUFFIX"
+        
+        echo "Renamed existing $USER_HOME/.cache folder  into $USER_HOME/.cache_backup_$RANDOM_SUFFIX"
     fi
     
     # check if storage already exist but avoid creating if already there
     if [ ! -f "$USER_HOME/.cache" ] || [ "$(readlink -f $USER_HOME/.cache)" != "$USER_CACHE" ]; then
         chpst -u $USER_NAME ln -sfn $USER_CACHE $USER_HOME/.cache
+        
+        echo "Created symlink $USER_HOME/.cache -> $USER_CACHE"
     fi
 fi
 
--- jetbrains-projector/etc/runit_init.d/30_jetbrains_projector.sh
+++ jetbrains-projector/etc/runit_init.d/30_jetbrains_projector.sh
@@ -26,62 +26,6 @@ export PROJECTOR_IDE=${PROJECTOR_IDE:-"PyCharm Professional Edition 2021.1.3"}
 export PROJECTOR_PORT=${PROJECTOR_PORT:-9999}
 export PROJECTOR_CONFIG="${HOSTNAME}-${PROJECTOR_CONFIG:-default}"
 
-PROJECTOR_CONFIG_FILE=$PROJECTOR_STORAGE/configs/$PROJECTOR_CONFIG/config.ini
-
-ensure_secure_config() {
-
-	DO_UPDATE=""
-	
-	# find authentication token (should be in section [PASSWORDS] and starts with 'password = ')
-	AUTH_TOKEN=$(grep -o "^password = .*" $PROJECTOR_CONFIG_FILE | cut -f2- -d=)
-	
-	# get user requested token if specified
-	USER_REQUESTED_TOKEN=$(cat $HOME/jetbrains-projector.token 2> /dev/null || echo "")
-	
-	# if auth token not configured then create new one
-	if [ -z "$AUTH_TOKEN" ]; then
-		# use user token if present
-		if [ -z "$USER_REQUESTED_TOKEN" ] ; then
-			AUTH_TOKEN=`date +%s | sha256sum | base64 | head -c 64`
-		else
-			AUTH_TOKEN=$USER_REQUESTED_TOKEN
-		fi
-		cat >> $PROJECTOR_CONFIG_FILE <<- EOF
-		[PASSWORDS]
-		password = $AUTH_TOKEN
-		ro_password = $AUTH_TOKEN
-		EOF
-		
-		DO_UPDATE=1
-	elif [ "$USER_REQUESTED_TOKEN" != "$AUTH_TOKEN" ] ; then
-		# password already present but user requested different one so change it
-		sed -i "s/password = .*/password = ${USER_REQUESTED_TOKEN}/g" $PROJECTOR_CONFIG_FILE
-		sed -i "s/ro_password = .*/ro_password = ${USER_REQUESTED_TOKEN}/g" $PROJECTOR_CONFIG_FILE
-		
-		DO_UPDATE=1
-	fi
-	
-	# find SSL token (should be in section [SSL] and starts with 'token = ')
-	SSL_TOKEN=$(grep -o "^token = .*" $PROJECTOR_CONFIG_FILE | cut -f2- -d=)
-	
-	# if SSL is not configured then create new token
-	if [ -z "$SSL_TOKEN" ]; then
-		SSL_TOKEN=`date +%s | sha256sum | base64 | head -c 20`
-		
-		cat >> $PROJECTOR_CONFIG_FILE <<- EOF
-		[SSL]
-		token = $SSL_TOKEN
-		EOF
-		
-		DO_UPDATE=1
-	fi
-	
-	# rebuild config file if changes were made
-	if [ ! -z "$DO_UPDATE" ]; then
-		chpst -u $USER_NAME $PROJECTOR_BIN config rebuild $PROJECTOR_CONFIG
-	fi 
-}
-
 mutex_instalation_lock() {
 	# wait until installation flag is finished
 	while [ -f "$USER_HOME/jetbrains-projector.installing" ]; do sleep 2; done	
@@ -112,8 +56,17 @@ fi
 if [ ! -z "$CREATE_PROJECTOR_STORAGE" ]; then 
 	chpst -u $USER_NAME mkdir -p $PROJECTOR_STORAGE
 	
+	# check if existing folder is not symlink but actual folder -- rename it in case user has something important there
+	if  [ -d "$USER_HOME/.projector" ] && [ ! -h "$USER_HOME/.projector" ] ; then
+		RANDOM_SUFFIX=$(date +%s | sha256sum | base64 | head -c 4)		
+		chpst -u $USER_NAME mv "$USER_HOME/.projector" "$USER_HOME/.projector_backup_$RANDOM_SUFFIX"
+		echo "Renamed existing $USER_HOME/.projector folder  into $USER_HOME/.projector_backup_$RANDOM_SUFFIX"
+	fi
+	
+	# check if storage already exist but avoid creating if already there
 	if [ ! -f "$USER_HOME/.projector" ] || [ "$(readlink -f $USER_HOME/.projector)" != "$PROJECTOR_STORAGE" ]; then
 		chpst -u $USER_NAME ln -sfn $PROJECTOR_STORAGE $USER_HOME/.projector
+		echo "Created symlink $USER_HOME/.projector -> $PROJECTOR_STORAGE"
 	fi
 fi
 
@@ -130,9 +83,6 @@ if [ -z "$($PROJECTOR_BIN config list | grep " ${PROJECTOR_CONFIG}$")" ]; then
 	chpst -u $USER_NAME $PROJECTOR_BIN autoinstall --config-name $PROJECTOR_CONFIG --ide-name "$PROJECTOR_IDE" --port $PROJECTOR_PORT --hostname 0.0.0.0
 	chpst -u $USER_NAME $PROJECTOR_BIN config update $PROJECTOR_CONFIG
 	echo "  (done)"
-	
-	# enable SSL and add tokens it not present
-	ensure_secure_config
 fi
 
 mutex_instalation_unlock
