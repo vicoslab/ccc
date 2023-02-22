c54385527b9dd65d298736a27a89c039294e6879
Domen Tabernik <domen.tabernik@fri.uni-lj.si>
2023-02-22 12:47:22 +0100

    Updated miniconda install in version v1.07:      - fixed bug in "conda clean" during instalation (invalid arg used)      - added instalation of mamba to base conda env during miniconda install

--- base/etc/setup_miniconda.sh
+++ base/etc/setup_miniconda.sh
@@ -3,9 +3,10 @@
 wget --quiet https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh -O ~/miniconda.sh && \
 /bin/bash ~/miniconda.sh -b -p ~/conda && \
 rm ~/miniconda.sh && \
-~/conda/bin/conda clean -tipsy && \
+~/conda/bin/conda clean -tipy && \
 echo ". ~/conda/etc/profile.d/conda.sh" >> ~/.bashrc && \
 echo "conda activate base" >> ~/.bashrc && \
 find ~/conda/ -follow -type f -name '*.a' -delete && \
 find ~/conda/ -follow -type f -name '*.js.map' -delete && \
-~/conda/bin/conda clean -afy
+~/conda/bin/conda clean -afy && ~/conda/bin/conda install mamba -n base -c conda-forge -y
+
